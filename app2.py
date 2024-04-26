02899de743940614d588bfd3d197c3929fc0043c
Nikita <nsr@flyfire.io>
2024-04-26 16:53:43 +0300

    Add authentication and demon checks

--- app2.py
+++ app2.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 # Configuration
 DOCKER_REGISTRY = "localhost:5100"
+REGISTRY_URL = f"https://{DOCKER_REGISTRY}/v2/"
 DOCKER_USERNAME = "username"
 DOCKER_PASSWORD = "3133"
 COMPOSE_FILE_PATH = 'docker-compose.yml'
@@ -52,6 +53,42 @@ def read_docker_compose_images(compose_path):
         compose_data = yaml.safe_load(file)
         images = [service['image'] for service in compose_data['services'].values() if 'image' in service]
         return images
+
+@app.route('/check_docker_daemon', methods=['POST'])
+def check_docker_daemon():
+    ret = "Checking connectivity to Docker daemon..." + "\n"
+    try:
+        response = client.ping()
+        if response:
+            ret += "Docker daemon is reachable."
+        else:
+            ret += "Docker daemon is not reachable."
+    except APIError as e:
+        ret += f"Failed to connect to Docker daemon: {str(e)}"
+    return render_template('index.html', ret1=ret)
+
+@app.route('/check_registry_reachability', methods=['POST'])
+def check_registry_reachability():
+    ret = "Checking connectivity to Docker registry..." + "\n"
+    try:
+        response = requests.get(REGISTRY_URL, timeout=5)
+        if response.status_code == 200:
+            ret += "Registry is reachable."
+        else:
+            ret += f"Registry is not reachable. Status code: {response.status_code}"
+    except requests.exceptions.RequestException as e:
+        ret += f"Failed to connect to the registry: {str(e)}"
+    return render_template('index.html', ret2=ret)
+
+@app.route('/authenticate_registry', methods=['POST'])
+def authenticate_registry():
+    ret = "Authenticating to Docker registry..." + "\n"
+    try:
+        client.login(username=DOCKER_USERNAME, password=DOCKER_PASSWORD, registry=DOCKER_REGISTRY)
+        ret += "Successfully authenticated."
+    except APIError as e:
+        ret += f"Authentication failed: {str(e)}"
+    return render_template('index.html', ret3=ret)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
